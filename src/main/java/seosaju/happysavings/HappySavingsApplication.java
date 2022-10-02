c2d143feeaecbd7bce2afd182bc7485f5c254af7
석상준 <sjseok@aiforpet.com>
2022-10-02 18:46:26 +0900

    프로젝트 생성

---
+++ src/main/java/seosaju/happysavings/HappySavingsApplication.java
@@ -0,0 +1,13 @@
+package seosaju.happysavings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappySavingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappySavingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/seosaju/happysavings/HappySavingsApplicationTests.java
@@ -0,0 +1,13 @@
+package seosaju.happysavings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappySavingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
