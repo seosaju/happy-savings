6b6dd865c0d1d984efcc9057b10b71127cbb4318
seosaju <qua_ver@naver.com>
2022-10-02 18:48:28 +0900

    유저 도메인 생성 및 JPA 연동

---
+++ src/main/java/seosaju/happysavings/module/user/domain/User.java
@@ -0,0 +1,40 @@
+package seosaju.happysavings.module.user.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @Email
+    private String email;
+
+    @Builder
+    public User(String name, String email) {
+        this.id = UUID.randomUUID().toString();
+        changeName(name);
+        this.email = email;
+    }
+
+    public void changeName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("이름은 반드시 있어야 합니다.");
+        }
+
+        this.name = name;
+    }
+}
