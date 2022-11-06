31e5ca9a2b298c3cb3d975fc26de063457e9969a
seosaju <qua_ver@naver.com>
2022-11-06 17:52:30 +0900

    공통 에러 처리 컨트롤러 추가

---
+++ src/main/java/seosaju/happysavings/infra/ErrorResponse.java
@@ -0,0 +1,20 @@
+package seosaju.happysavings.infra;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorResponse {
+
+    private String code;
+
+    private String message;
+
+    @Builder
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
